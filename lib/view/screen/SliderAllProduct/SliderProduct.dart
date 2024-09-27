e36da0c7e9e4f3b6a1269a4305c8a4480c72ae15
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-28 00:37:11 +0600

    pc mart version 2

--- lib/view/screen/SliderAllProduct/SliderProduct.dart
+++ lib/view/screen/SliderAllProduct/SliderProduct.dart
@@ -32,89 +32,88 @@ class SliderProductScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Expanded(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 30,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: SearchField(
+                onChanged: (String text) {
+                  controller.searchFunction(searchText: text);
+                },
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: SearchField(
-                  onChanged: (String text) {
-                    log("===== onChanged : $text ==================");
-                    controller.searchFunction(searchText: text);
-                  },
-                ),
-              ),
-              const SizedBox(height: 10),
-              Obx(() => controller.isLoading.isTrue
-                  ? const Center(child: CircularProgressIndicator())
-                  : controller.productList.isEmpty
-                  ? const Center(
-                  child: CommonText(title: "Empty Product List"))
-                  : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: 10),
-                    child: GridView.builder(
-                      physics: const PageScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: controller.productList.length,
-                      gridDelegate:
-                      const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                      ),
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            Get.to(() => ProductInfo(
-                                id: index,
-                                productData:
-                                controller.productList[index]));
-                          },
-                          child: Card(
-                            elevation: 4,
-                            color: Colors.white,
-                            child: SizedBox(
-                              height: 220,
-                              width:
-                              MediaQuery.sizeOf(context).width /
-                                  2.2,
-                              child: Column(
-                                crossAxisAlignment:
-                                CrossAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                      height: 100,
-                                      width: 100,
-                                      child: Image(
-                                          image: AssetImage(
-                                              "${controller.productList[index].image}"))),
-                                  CommonText(
-                                      title:
-                                      "ID : ${controller.productList[index].productId}"),
-                                  CommonText(
-                                      title:
-                                      "Name : ${controller.productList[index].nameEn}"),
-                                  CommonText(
-                                      title:
-                                      "Price : ${controller.productList[index].regPrice}Tk"),
-                                ],
-                              ),
+            ),
+            SizedBox(height: 10),
+            Text("ALL PRODUCT",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: Color(0xff9a0000))),
+            const SizedBox(height: 10),
+            Obx(() => controller.isLoading.isTrue
+                ? const Center(child: CircularProgressIndicator())
+                : controller.productList.isEmpty
+                ? const Center(
+                child: CommonText(title: "Empty Product List"))
+                : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                  const EdgeInsets.symmetric(horizontal: 10),
+                  child: GridView.builder(
+                    physics: const PageScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: controller.productList.length,
+                    gridDelegate:
+                    const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemBuilder: (context, index) {
+                      return InkWell(
+                        onTap: () {
+                          Get.to(() => ProductInfo(
+                              id: index,
+                              productData:
+                              controller.productList[index]));
+                        },
+                        child: Card(
+                          elevation: 4,
+                          color: Colors.white,
+                          child: SizedBox(
+                            height: 220,
+                            width:
+                            MediaQuery.sizeOf(context).width /
+                                2.2,
+                            child: Column(
+                              crossAxisAlignment:
+                              CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                    height: 100,
+                                    width: 100,
+                                    child: Image(
+                                        image: AssetImage(
+                                            "${controller.productList[index].image}"))),
+                                CommonText(
+                                    title:
+                                    "ID : ${controller.productList[index].productId}"),
+                                CommonText(
+                                    title:
+                                    "Name : ${controller.productList[index].nameEn}"),
+                                CommonText(
+                                    title:
+                                    "Price : ${controller.productList[index].regPrice}Tk"),
+                              ],
                             ),
                           ),
-                        );
-                      },
-                    ),
+                        ),
+                      );
+                    },
                   ),
-                ],
-              )),
-            ],
-          ),
+                ),
+              ],
+            )),
+          ],
         ),
       ),
     );
