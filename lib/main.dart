e2e2693b66661dc81e212b8270d12299ded2c350
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-01 23:42:00 +0600

    pc mart version 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:pc_shop_version02/view/screen/Profile/profile.dart';
+import 'package:pc_shop_version02/view/screen/home%20api%20version/home.dart';
 import 'package:pc_shop_version02/view/screen/splash/splash.dart';
 void main(){
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(debugShowCheckedModeBanner: false,
-      home: Profile(),
+      home: Home(),
         builder: EasyLoading.init(),
     );
   }
