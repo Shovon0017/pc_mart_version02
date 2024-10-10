f98580cd4cac5c613b80282dcddcf5f590746c25
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-10 22:26:03 +0600

    pc mart version 2

--- lib/view/screen/updatePage.dart
+++ lib/view/screen/updatePage.dart
@@ -8,7 +8,22 @@ class UpdatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: const Color(0xffFFFFFF),
+        title: SizedBox(
+            height: 32,
+            width: 114,
+            child: Image.asset("images/pcmart.jpg")),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: CommonIconButton(onTap: (){
+              Get.to(()=>const NotificationShow());
+            }),
+          )
+        ],
+      ),
     );
   }
 }
