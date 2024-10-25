6b48960c7e7d7805e2e80128a0262460d955ae22
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-25 22:09:59 +0600

    pc mart solving

--- lib/view/screen/OrderTrack/OrderTrack.dart
+++ lib/view/screen/OrderTrack/OrderTrack.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pc_shop_version02/common%20widget/CommonIcon.dart';
@@ -31,14 +32,181 @@ class _OrderTrackState extends State<OrderTrack> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          const Text("Percel Track",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-          const SizedBox(
-            height: 100,
-          ),
-          Image.asset("images/no-product-found.png"),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text("Order Track",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 180,
+              width: double.infinity,
+              child:
+              Card(
+                color: Color(0xff9a0000),
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("On The Way To China!",style: TextStyle(fontSize: 30,color: Colors.white),),
+                    Text("Estimated Delivery Date Is",style: TextStyle(fontSize: 30,color: Colors.white),),
+                    Text("23 October - 29 October",style: TextStyle(fontSize: 30,color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 80,
+              width: double.infinity,
+              child:
+              Card(
+                color: CupertinoColors.systemGrey4,
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                        text: const TextSpan(children: [
+                          TextSpan(
+                              text: "Delivery Partner :",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                          TextSpan(
+                              text: "BD-DEX",
+                              style: TextStyle(fontSize: 20, color: Colors.black))
+                        ])),
+                    RichText(
+                        text: const TextSpan(children: [
+                          TextSpan(
+                              text: "Tracking Number:",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                          TextSpan(
+                              text: "DEX-BND-00000981234",
+                              style: TextStyle(fontSize: 20, color: Color(0xff9a0000)))
+                        ])),
+
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 80,
+              width: double.infinity,
+              child:
+              Card(
+                color: CupertinoColors.systemGrey4,
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                        text: const TextSpan(children: [
+                          TextSpan(
+                              text: "Receiver Name:",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                          TextSpan(
+                              text: "User-001",
+                              style: TextStyle(fontSize: 20, color: Colors.black))
+                        ])),
+                    RichText(
+                        text: const TextSpan(children: [
+                          TextSpan(
+                              text: "Address:",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black)),
+                          TextSpan(
+                              text: "House 09,Road 5,Bheramara,Kushtia",
+                              style: TextStyle(fontSize: 20, color: Colors.black,overflow: TextOverflow.ellipsis))
+                        ])),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child:
+              Card(
+                  color: CupertinoColors.systemGrey4,
+                  child: Obx(
+                        () => ListView.builder(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        shrinkWrap: true,
+                        itemCount: controller.cart.length,
+                        itemBuilder: (_, index) {
+                          return Card(
+                            margin: const EdgeInsets.symmetric(vertical: 5),
+                            surfaceTintColor: Colors.transparent,
+                            shape: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey.withOpacity(.3), strokeAlign: BorderSide.strokeAlignOutside),
+                            ),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                    height: 80,
+                                    width:80,child: Image.asset("${controller.cart[index].image}")),
+
+                                Expanded(
+                                  flex: 5,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "${controller.cart[index].nameEn}",style: TextStyle(fontSize: 20,overflow: TextOverflow.ellipsis),),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "Price : ",style: TextStyle(
+                                              fontSize: 20,
+                                            ),
+                                            ),
+                                            Text(
+                                              "${controller.cart[index].regPrice} ৳",style: TextStyle(
+                                              fontSize: 20,
+                                            ),
+
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Spacer(),
+                                            InkWell(
+                                              onTap: () async {
+                                                controller.cart.removeAt(index);
+                                              },
+                                              child: InkWell(onTap: (){
+                                                Get.offAll(()=>NavigationBarShow());
+                                              },
+                                                  child: CommonButton(buttonName: "Buy Again",buttonColor: Color(0xff9a0000),textColor: Colors.white,buttonHeight: 25,buttonWidth: 120, onTap:(){
+                                                    Get.offAll(()=>NavigationBarShow());
+                                                  })
+                                              ),
+                                            ),
+                                            const SizedBox(width: 10)
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
+                  )
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
