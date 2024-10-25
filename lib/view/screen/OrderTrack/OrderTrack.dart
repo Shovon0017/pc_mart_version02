fd3c1e39dd68f1c02bc2b52f832b8617abfbaeb6
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-25 22:16:37 +0600

    pc mart solving

--- lib/view/screen/OrderTrack/OrderTrack.dart
+++ lib/view/screen/OrderTrack/OrderTrack.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pc_shop_version02/common%20widget/CommonIcon.dart';
+import 'package:pc_shop_version02/common%20widget/common_button.dart';
+import 'package:pc_shop_version02/controller/getX%20controller/product_Info.dart';
+import 'package:pc_shop_version02/view/screen/navigation%20bar/navigation.dart';
 import 'package:pc_shop_version02/view/screen/notification/notification.dart';
 
 class OrderTrack extends StatefulWidget {
@@ -14,6 +17,8 @@ class OrderTrack extends StatefulWidget {
 class _OrderTrackState extends State<OrderTrack> {
   @override
   Widget build(BuildContext context) {
+
+    ProductInfoController controller=Get.put(ProductInfoController());
     return Scaffold(backgroundColor: const Color(0xffFFFFFF),
 
       appBar: AppBar(
@@ -38,21 +43,6 @@ class _OrderTrackState extends State<OrderTrack> {
             const Text("Order Track",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
             SizedBox(
               height: 20,
-            ),
-            SizedBox(
-              height: 180,
-              width: double.infinity,
-              child:
-              Card(
-                color: Color(0xff9a0000),
-                child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("On The Way To China!",style: TextStyle(fontSize: 30,color: Colors.white),),
-                    Text("Estimated Delivery Date Is",style: TextStyle(fontSize: 30,color: Colors.white),),
-                    Text("23 October - 29 October",style: TextStyle(fontSize: 30,color: Colors.white)),
-                  ],
-                ),
-              ),
             ),
             SizedBox(
               height: 80,
@@ -127,84 +117,7 @@ class _OrderTrackState extends State<OrderTrack> {
                 ),
               ),
             ),
-            SizedBox(
-              height: 300,
-              width: double.infinity,
-              child:
-              Card(
-                  color: CupertinoColors.systemGrey4,
-                  child: Obx(
-                        () => ListView.builder(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        shrinkWrap: true,
-                        itemCount: controller.cart.length,
-                        itemBuilder: (_, index) {
-                          return Card(
-                            margin: const EdgeInsets.symmetric(vertical: 5),
-                            surfaceTintColor: Colors.transparent,
-                            shape: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.grey.withOpacity(.3), strokeAlign: BorderSide.strokeAlignOutside),
-                            ),
-                            child: Row(
-                              children: [
-                                SizedBox(
-                                    height: 80,
-                                    width:80,child: Image.asset("${controller.cart[index].image}")),
 
-                                Expanded(
-                                  flex: 5,
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        Text(
-                                          "${controller.cart[index].nameEn}",style: TextStyle(fontSize: 20,overflow: TextOverflow.ellipsis),),
-                                        Row(
-                                          children: [
-                                            Text(
-                                              "Price : ",style: TextStyle(
-                                              fontSize: 20,
-                                            ),
-                                            ),
-                                            Text(
-                                              "${controller.cart[index].regPrice} ৳",style: TextStyle(
-                                              fontSize: 20,
-                                            ),
-
-                                            ),
-                                          ],
-                                        ),
-                                        Row(
-                                          children: [
-                                            Spacer(),
-                                            InkWell(
-                                              onTap: () async {
-                                                controller.cart.removeAt(index);
-                                              },
-                                              child: InkWell(onTap: (){
-                                                Get.offAll(()=>NavigationBarShow());
-                                              },
-                                                  child: CommonButton(buttonName: "Buy Again",buttonColor: Color(0xff9a0000),textColor: Colors.white,buttonHeight: 25,buttonWidth: 120, onTap:(){
-                                                    Get.offAll(()=>NavigationBarShow());
-                                                  })
-                                              ),
-                                            ),
-                                            const SizedBox(width: 10)
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        }),
-                  )
-              ),
-            )
           ],
         ),
       ),
