a65a83ec5355beda9dc97997367f49a303b7d215
Shovon0017 <Shimantomostofa@gmail.com>
2024-10-26 22:53:13 +0600

    pc mart solving

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:pc_shop_version02/view/screen/Order%20%20report/Order_report.dart';
+import 'package:pc_shop_version02/view/screen/OrderTrack/OrderTrack.dart';
 import 'package:pc_shop_version02/view/screen/navigation%20bar/navigation.dart';
 import 'package:pc_shop_version02/view/screen/requested_order/requested_order.dart';
 import 'package:pc_shop_version02/view/screen/splash/splash.dart';
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(debugShowCheckedModeBanner: false,
-      home: NavigationBarShow(),
+      home: OrderTrack(),
         builder: EasyLoading.init(),
     );
   }
